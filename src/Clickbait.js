5e461da3ab293f1720456f8c5d99fa42fa778fb3
Stephen Crowell <crowelsl@rose-hulman.edu>
2018-06-11 19:30:51 -0400

    render bottom ads by using a map.

---
+++ src/Clickbait.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class Clickbait extends Component {
+    render() {
+        return (
+            <div className="small-6 medium-3 columns other-article">
+                    <a href="#">
+                        <img src={this.props.img} alt={this.props.alt} />
+                        <p>{this.props.text}</p>
+                    </a>  
+            </div>
+        )
+    }
+}
+
+export default Clickbait;
