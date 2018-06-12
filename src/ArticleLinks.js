13d2a37790e6c4db9cae64442a92562f417d07cc
Stephen Crowell <crowelsl@rose-hulman.edu>
2018-06-11 20:10:23 -0400

    now Commetns are sperate component

--- src/ArticleLinks.js
+++ src/ArticleLinks.js
@@ -2,11 +2,32 @@ import React, {Component} from 'react';
 
 import Comments from './Comments'
 class AtricleLinks extends Component {
+    constructor() {
+        super();
+        this.state = {
+            isVisible: false,
+        }
+    }
     render() {
         return (
-            <Comments />
+            <div className="article-links">
+                <button className="article-link" href="#" onClick={() => this.showComments()}>
+                    <i className="fa fa-comments-o"></i>
+                    <span className="article-link-text">Comments</span>
+                </button>
+                <a className="article-link" href="#">
+                    <i className="fa fa-share"></i>
+                    <span className="article-link-text">Share Post</span>
+                </a>
+                {this.state.isVisible ? <Comments />: null}
+            </div>
         )
     }
+
+    showComments() {
+        this.setState({isVisible:  (this.state.isVisible ? false : true)});
+    }
+
 }
 
 export default AtricleLinks;
--- src/Comments.js
+++ src/Comments.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 
+
 class Comments extends Component {
     constructor() {
         super();
@@ -9,32 +10,18 @@ class Comments extends Component {
     }
     render() {
         return (
-            <div className="article-links">
-                <button className="article-link" href="#" onClick={() => this.showComments()}>
-                    <i className="fa fa-comments-o"></i>
-                    <span className="article-link-text">Comments</span>
-                </button>
-                <a className="article-link" href="#">
-                    <i className="fa fa-share"></i>
-                    <span className="article-link-text">Share Post</span>
-                </a>
-                {this.state.isVisible ? (
-                    <div>
-                        <form onSubmit={() => this.addComment()}>
-                            <input type='text' name='comment'></input>
-                            <input type='submit'></input>
-                        </form>
-                        <ul></ul>
-                    </div>    
-                    )
-                    : null
-                }
+            <div>
+                <form onSubmit={() => this.addComment()}>
+                    <input type='text' name='comment'></input>
+                    <input type='submit'></input>
+                </form>
+                <ul></ul>   
             </div>
         )
     }
 
-    showComments() {
-        this.setState({isVisible:  (this.state.isVisible ? false : true)});
+    addComment() {
+        
     }
 
 }
