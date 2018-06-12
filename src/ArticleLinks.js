886897eec8f62a0c7f53fdc43bc82c7ea8e1a8ee
Stephen Crowell <crowelsl@rose-hulman.edu>
2018-06-11 23:41:05 -0400

    Comments no longer disappear

--- src/ArticleLinks.js
+++ src/ArticleLinks.js
@@ -5,27 +5,29 @@ class AtricleLinks extends Component {
     constructor() {
         super();
         this.state = {
-            isVisible: false,
+            isVisible: 'inline',
         }
     }
     render() {
         return (
             <div className="article-links">
-                <button className="article-link" href="#" onClick={() => this.showComments()}>
+                <a className="article-link" href="#" onClick={(e) => this.showComments(e)}>
                     <i className="fa fa-comments-o"></i>
                     <span className="article-link-text">Comments</span>
-                </button>
+                </a>
                 <a className="article-link" href="#">
                     <i className="fa fa-share"></i>
                     <span className="article-link-text">Share Post</span>
                 </a>
-                {this.state.isVisible ? <Comments />: null}
+                <Comments />
             </div>
         )
     }
 
-    showComments() {
-        this.setState({isVisible:  (this.state.isVisible ? false : true)});
+    showComments(e) {
+        e.preventDefault();
+        this.setState({isVisible:  (this.state.isVisible == 'inline' ? 'none' : 'inline')});
+        document.querySelector('.comments').style.display = this.state.isVisible;
     }
 
 }
--- src/Comments.js
+++ src/Comments.js
@@ -16,9 +16,9 @@ class Comments extends Component {
     }
     render() {
         return (
-            <div>
+            <div className='comments'>
                 <form onSubmit={(e) => this.addComment(e)}>
-                    <input type='text' value={this.state.newComment} onChange={(e) => this.handleChange(e)}></input>
+                    <input type='text' value={this.state.newComment} placeholder='Enter your comment' onChange={(e) => this.handleChange(e)}></input>
                     <input type='submit'></input>
                 </form>
                 <ul>{this.state.comments.map(comment => <Comment comment={comment}/>)}</ul>   
@@ -35,6 +35,7 @@ class Comments extends Component {
         const comments = [...this.state.comments];
         comments.push(this.state.newComment);
         this.setState({comments});
+        this.setState({newComment: ''});
     }
 
 }
