47e823751b95c002347f84fe9f4b2a7ef97a3376
Stephen Crowell <crowelsl@rose-hulman.edu>
2018-06-11 20:39:25 -0400

    sumbit comments

--- src/Comments.js
+++ src/Comments.js
@@ -1,27 +1,40 @@
 import React, {Component} from 'react';
 
+class Comment extends Component {
+    render() {
+      return <li>{this.props.comment}</li>
+    }
+}
 
 class Comments extends Component {
     constructor() {
         super();
         this.state = {
-            isVisible: false,
+            comments: [],
+            newComment: '',
         }
     }
     render() {
         return (
             <div>
-                <form onSubmit={() => this.addComment()}>
-                    <input type='text' name='comment'></input>
+                <form onSubmit={(e) => this.addComment(e)}>
+                    <input type='text' value={this.state.newComment} onChange={(e) => this.handleChange(e)}></input>
                     <input type='submit'></input>
                 </form>
-                <ul></ul>   
+                <ul>{this.state.comments.map(comment => <Comment comment={comment}/>)}</ul>   
             </div>
         )
     }
 
-    addComment() {
-        
+    handleChange(event) {
+        this.setState({newComment: event.target.value});
+    }
+
+    addComment(e) {
+        e.preventDefault();
+        const comments = [...this.state.comments];
+        comments.push(this.state.newComment);
+        this.setState({comments});
     }
 
 }
